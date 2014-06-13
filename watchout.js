bdcab385c74e8e2542e692a77e71c9fc93f37208
Hack Reactor Students <alexander.gugel@gmail.com>
2014-06-13 15:04:11 -0700

    Make it so that the enemies move to a new random location every two seconds.

--- watchout.js
+++ watchout.js
@@ -1,33 +1,51 @@
-// start slingin' some d3 here.
-
 var width = 700;
 var height = 450;
-var numEnemies = 50;
+var numEnemies = 35;
 
 var svg = d3.select('body').append('svg')
   .attr('width', width)
-  .attr('height',height);
+  .attr('height', height);
 
 var enemies = [];
 
-for (var i = 0; i < numEnemies; i++) {
-  enemies.push({
-    x: Math.floor(width*Math.random()),
-    y: Math.floor(height*Math.random()),
-    r: 10
-  });
-}
+var updateEnemiesData = function () {
+  for (var i = 0; i < numEnemies; i++) {
+    enemies[i] = {
+      x: Math.floor(width*Math.random()),
+      y: Math.floor(height*Math.random()),
+      r: 10
+    };
+  }
+};
+var update = function(){
+  updateEnemiesData();
+
+  svg.selectAll('circle')
+    .data(enemies)
+    .transition()
+    .duration(800)
+    .attr('cx', function (d) {
+      return d.x;
+    })
+    .attr('cy', function (d) {
+      return d.y;
+    })
+    .attr('r', function (d) {
+      return d.r;
+    });
+};
 
 
-svg.selectAll('circle.enemy').data(enemies)
+updateEnemiesData();
+svg.selectAll('circle')
+  .data(enemies)
   .enter()
-  .append('circle')
-  .attr('cx', function (d) {
-    return d.x;
-  })
-  .attr('cy', function (d) {
-    return d.y;
-  })
-  .attr('r', function (d) {
-    return d.r;
-  });
+  .append('circle');
+update();
+
+
+setInterval(update, 2000);
+
+
+
+
