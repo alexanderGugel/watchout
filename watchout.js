c97b4fcfa1d77a4fff039d4395355989ca3ac7e5
Hack Reactor Students <alexander.gugel@gmail.com>
2014-06-13 14:46:53 -0700

    Draw the enemies in an svg element

--- watchout.js
+++ watchout.js
@@ -1,2 +1,33 @@
 // start slingin' some d3 here.
 
+var width = 700;
+var height = 450;
+var numEnemies = 50;
+
+var svg = d3.select('body').append('svg')
+  .attr('width', width)
+  .attr('height',height);
+
+var enemies = [];
+
+for (var i = 0; i < numEnemies; i++) {
+  enemies.push({
+    x: Math.floor(width*Math.random()),
+    y: Math.floor(height*Math.random()),
+    r: 10
+  });
+}
+
+
+svg.selectAll('circle.enemy').data(enemies)
+  .enter()
+  .append('circle')
+  .attr('cx', function (d) {
+    return d.x;
+  })
+  .attr('cy', function (d) {
+    return d.y;
+  })
+  .attr('r', function (d) {
+    return d.r;
+  });
